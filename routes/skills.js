eaf4f591f3e0b4a28c94b63626cf34f44e165718
Thomas Nix <tnix2@uncc.edu>
2017-08-17 10:39:30 -0400

    Added structure for dynamic loading of skills info and laid out structure of EJS rendering, passing in values.

--- routes/skills.js
+++ routes/skills.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 // Skills page - INDEX
 router.get('/', function(req,res){
-	res.render("skills/index");
+	res.render("skills/index",
+		{
+			skillsCardInfo: {
+				front: "Front end skills are those that influence how a web page works on the client side. This includes HTML, CSS, and JavaScript, as well as libraries or frameworks for each.",
+				back: "Back end skills are those that change how a client interacts with a server, and how data is processed and sent back to the client. This includes routing, database interaction, authentication, etc.",
+				other: "Other skills are those that either don\\'t fit neatly into either front or back end skills, but are still important. Examples are workflow, design structure, team coordination, etc."
+			}
+		});
 });
 // Skills page - NEW
 router.get('/new', function(req,res){});
