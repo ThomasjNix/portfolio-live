582c8cc43cd0b1abafe479ad5dbb777998958831
Thomas Nix <tnix2@uncc.edu>
2017-08-20 21:51:51 -0400

    Had my portfolio reviewed and implemented some stylistic changes, including adding a proper viewport meta tag and page blurring on mobile navigation click

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -3,3 +3,13 @@
 })(window.location.pathname.substr(1));
 
 $(".button-collapse").sideNav();
+
+$('#mobile-menu-icon').on('click', function(){
+	$('body > div.drag-target').css('display','initial');
+	$('#main-content-container').css('filter','blur(5px)');
+
+});
+$('body > div.drag-target').on('click', function(){
+	$('body > div.drag-target').css('display','none');
+	$('#main-content-container').css('filter','none');
+});
